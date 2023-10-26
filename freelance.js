02f986150cf60b58a53060178ec32fffd84da45d
Neha <nehad2000@gmail.com>
2023-10-26 13:33:14 -0500

    initial commit

---
+++ freelance.js
@@ -0,0 +1,10 @@
+const freelancers = [
+  { name: 'Alice', occupation: 'Writer', price: 30},
+  { name: 'Bob', occupation: 'Teacher', price: 50},
+  { name: 'Carol', occupation: 'Programmer', price: 70},
+  { name: 'Katie', occupation: 'Dancer', price: 35},
+  { name: 'Bella', occupation: 'Teacher', price: 40},
+  { name: 'Taylor', occupation: 'Programmer', price: 20},
+  { name: 'Anna', occupation: 'Writer', price: 85},
+  { name: 'Jordan', occupation: 'Writer', price: 45},
+]
